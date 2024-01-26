70b78a84c6d489dafd3c94485c0b94c14fceb21b
Выгановский Глеб Юрьевич <322-20@std.pkgh.edu>
2024-01-26 13:46:18 +0300

    img + filter

--- general/views.py
+++ general/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from flowershop.models import Product
+
 
 def index(request):
-    return render(request, 'index.html')
+    product = Product.objects.filter(counts__gt=0)
+    return render(request, 'index.html', context={'product': product})
